6e7ffe41e2f0b7e195816df69ff171dc1d69fae0
g-vega-cl <g.vega.cl@gmail.com>
2021-02-07 16:01:24 -0600

    Functional components

--- 2-Front-End Web Development with React/React/confusion/src/components/DishdetailComponent.js
+++ 2-Front-End Web Development with React/React/confusion/src/components/DishdetailComponent.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
     Card,
     CardImg,
@@ -6,54 +7,55 @@ import {
     CardTitle,
   } from "reactstrap";
 
-export const DishDetail = ({dish}) => {
+const RenderDish = ({dish}) =>{
+    return(
+    <div className="col-12 col-md-5 m-1" >
+    <Card>
+        <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+        <CardBody>
+        <CardTitle>{dish.name}</CardTitle>
+        <CardText>{dish.description}</CardText>
+        </CardBody>
+    </Card>
+    </div>
+    );
+}
 
-    let renderDish = (dish) =>{
-        return(
-        <div className="col-12 col-md-5 m-1" >
-        <Card>
-          <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-        </div>
-        );
-    }
+const RenderComments = ({commentsArray}) => {
+    const comments = commentsArray.map((comment) => {
 
-    let renderComments = (commentsArray) => {
-        const comments = commentsArray.map((comment) => {
-
-            return (
-                
-                <div key={comment.id} className="col-12">
-                    <h4>Comment</h4>
-                    <ul className="list-unstyled">
-                        <li><p>{comment.comment}</p></li>
-                        <li></li>
-                        <li><p>--{comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p></li>
-                    </ul>
-                </div>
-                
-                
-            );
-        })
-        return(
-            <div>
-                {comments}
+        return (
+            
+            <div key={comment.id} className="col-12">
+                <h4>Comment</h4>
+                <ul className="list-unstyled">
+                    <li><p>{comment.comment}</p></li>
+                    <li></li>
+                    <li><p>--{comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p></li>
+                </ul>
             </div>
             
+            
         );
-    }
-
-
+    })
+    return(
+        <div>
+            {comments}
+        </div>
+        
+    );
+}
+export const DishDetail = ({dish}) => {
     if (dish != null) {
       return (
         <div className="container">
         <div className="row">
-        {renderDish(dish)}
-        {renderComments(dish.comments)}
+            <RenderDish
+            dish={dish}
+            />
+            <RenderComments
+            commentsArray = {dish.comments}
+            />
         </div>
         </div>
       );
--- 2-Front-End Web Development with React/React/confusion/src/components/MenuComponent.js
+++ 2-Front-End Web Development with React/React/confusion/src/components/MenuComponent.js
@@ -7,21 +7,31 @@ import {
     CardImgOverlay,
   } from "reactstrap";
 
-export const Menu = ({dishes, onClick}) =>{
-    
 
-    const menu = dishes.map((dish) => {
-        return (
-          <div key={dish.id} className="col-12 col-md-5 m-1">
-            <Card onClick={()=> onClick(dish.id)}>
-              <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
-              <CardImgOverlay>
-                <CardTitle heading>{dish.name}</CardTitle>
-              </CardImgOverlay>
-            </Card>
-          </div>
+
+const RenderMenuItem = ({dish, onClick}) =>{
+    return (
+          <Card onClick={()=> onClick(dish.id)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+            <CardImgOverlay>
+              <CardTitle heading>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
+      );
+}
+
+export const Menu = (props) =>{
+    const menu = props.dishes.map((dish) => {
+        return(
+        <div key={dish.id} className="col-12 col-md-5 m-1">
+            <RenderMenuItem
+                dish={dish}
+                onClick ={props.onClick}
+            />
+        </div>
         );
       });
+      
       return (
         <div className="container">
           <div className="row">{menu}</div>
