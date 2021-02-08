32cd4f56e1108083895865c12409605202885cfb
g-vega-cl <g.vega.cl@gmail.com>
2021-02-07 19:25:39 -0600

    Header and footer

--- 2-Front-End Web Development with React/React/confusion/src/components/MainComponent.js
+++ 2-Front-End Web Development with React/React/confusion/src/components/MainComponent.js
@@ -1,20 +1,20 @@
-import React, {useState} from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
-import {Menu} from "./MenuComponent";
-import {DishDetail} from "../components/DishdetailComponent";
-import {dishes} from "../shared/dishes";
+import React, { useState } from "react";
+import { Menu } from "./MenuComponent";
+import { DishDetail } from "../components/DishdetailComponent";
+import { Header } from "./HeaderComponent";
+import { Footer } from "./FooterComponent";
+import { dishes } from "../shared/dishes";
 
 export const Main = () => {
-    const [selectedDishId, onDishSelectId] = useState(null);
-    return (
-        <div>
-          <Navbar dark color="primary">
-            <div className="container">
-              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-            </div>
-          </Navbar>
-          <Menu dishes={dishes} onClick={(dishId) => onDishSelectId(dishId)} />
-          <DishDetail dish={dishes.filter((dish) => dish.id === selectedDishId)[0]} />
-        </div>
-    );
-}
+  const [selectedDishId, onDishSelectId] = useState(null);
+  return (
+    <div>
+      <Header />
+      <Menu dishes={dishes} onClick={(dishId) => onDishSelectId(dishId)} />
+      <DishDetail
+        dish={dishes.filter((dish) => dish.id === selectedDishId)[0]}
+      />
+      <Footer/>
+    </div>
+  );
+};
