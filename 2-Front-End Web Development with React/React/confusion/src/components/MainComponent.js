903974d2827ba149e9e5e17c2baa4a4f3ea8f815
g-vega-cl <g.vega.cl@gmail.com>
2021-02-08 18:44:55 -0600

    Single page applications part 1

--- 2-Front-End Web Development with React/React/confusion/src/components/MainComponent.js
+++ 2-Front-End Web Development with React/React/confusion/src/components/MainComponent.js
@@ -1,22 +1,41 @@
 import React, { useState } from "react";
 import { Home } from "./HomeComponent";
 import { Menu } from "./MenuComponent";
+import Contact from "./ContactComponent";
 import { DishDetail } from "../components/DishdetailComponent";
 import { Header } from "./HeaderComponent";
 import { Footer } from "./FooterComponent";
-import { dishes } from "../shared/dishes";
+import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 export const Main = () => {
+
+  const [dishes, setDishes] = useState(DISHES);
+  const [promotions, setPromotions] = useState(PROMOTIONS);
+  const [leaders, setLeaders] = useState(LEADERS);
+
   const HomePage = () => {
-    return <Home />;
+    return (
+      <Home 
+      dish={dishes.filter((dish)=>dish.featured)[0]}
+      promotion={promotions.filter((promotion)=>promotion.featured)[0]}
+      leader={leaders.filter((leader)=>leader.featured)[0]}
+      />
+
+    
+    );
   };
+  
   return (
     <div>
       <Header />
       <Switch>
         <Route path="/home" component={HomePage} />
-        <Route exact path="/menu" component={() => <Menu dishes={dishes} />} />
+        <Route exact path="/menu" component={() => <Menu dishes={DISHES} />} />
+        <Route exact path = "/contactus" component = {Contact} />
         <Redirect to="/home"></Redirect>
       </Switch>
 
--- 2-Front-End Web Development with React/React/confusion/src/shared/dishes.js
+++ 2-Front-End Web Development with React/React/confusion/src/shared/dishes.js
@@ -1,48 +1,43 @@
-
-export const dishes = [
-    {
-      id: 0,
-      name: "Uthappizza",
-      image: "assets/images/uthappizza.png",
-      category: "mains",
-      label: "Hot",
-      price: "4.99",
-      description:
-        "A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
-        comments: [{id: "zasdasb", date: "2020-07-01", comment:"Imagine all the eatables, living in confusion", author: "Jhon lenny"}],
-    },
-    {
-      id: 1,
-      name: "Zucchipakoda",
-      image: "assets/images/zucchipakoda.png",
-      category: "appetizer",
-      label: "",
-      price: "1.99",
-      description:
-        "Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce",
-      comments: [{id: "zasdasb", date: "2020-07-01", comment:"Imagine all the eatables, living in confusion", author: "Jhon lenny"}],
-
-    },
-    {
-      id: 2,
-      name: "Vadonut",
-      image: "assets/images/vadonut.png",
-      category: "appetizer",
-      label: "New",
-      price: "1.99",
-      description:
-        "A quintessential ConFusion experience, is it a vada or is it a donut?",
-        comments: [{id: "asdasb", date: "2020-07-01", comment:"Imagine all the eatables, living in confusion", author: "Jhon lenny"}],
-    },
-    {
-      id: 3,
-      name: "ElaiCheese Cake",
-      image: "assets/images/elaicheesecake.png",
-      category: "dessert",
-      label: "",
-      price: "2.99",
-      description:
-        "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
-        comments: [{id: "asdas", date: "2020-07-01", comment:"Imagine all the eatables, living in confusion", author: "Jhon lenny"}],
-    },
-  ];
+export const DISHES =
+    [
+        {
+        id: 0,
+        name:'Uthappizza',
+        image: '/assets/images/uthappizza.png',
+        category: 'mains',
+        label:'Hot',
+        price:'4.99',
+        featured: true,
+        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                    
+        },
+        {
+        id: 1,
+        name:'Zucchipakoda',
+        image: '/assets/images/zucchipakoda.png',
+        category: 'appetizer',
+        label:'',
+        price:'1.99',
+        featured: false,
+        description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
+        },
+        {
+        id: 2,
+        name:'Vadonut',
+        image: '/assets/images/vadonut.png',
+        category: 'appetizer',
+        label:'New',
+        price:'1.99',
+        featured: false,
+        description:'A quintessential ConFusion experience, is it a vada or is it a donut?'
+        },
+        {
+        id: 3,
+        name:'ElaiCheese Cake',
+        image: '/assets/images/elaicheesecake.png',
+        category: 'dessert',
+        label:'',
+        price:'2.99',
+        featured: false,
+        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+        }
+    ];
