a818c21abeef76d9cc9bdaac8f1684baa35cbd58
g-vega-cl <g.vega.cl@gmail.com>
2021-02-12 15:37:27 -0600

    Assignment week 3 redux

--- 2-Front-End Web Development with React/React/confusion/src/components/DishdetailComponent.js
+++ 2-Front-End Web Development with React/React/confusion/src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import {
+  Button,
   Breadcrumb,
   BreadcrumbItem,
   Card,
@@ -7,8 +8,14 @@ import {
   CardText,
   CardBody,
   CardTitle,
+  Col,
+  Label,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Row,
 } from "reactstrap";
-
+import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
 
 const RenderDish = ({ dish }) => {
@@ -26,31 +33,138 @@ const RenderDish = ({ dish }) => {
 };
 
 const RenderComments = ({ commentsArray }) => {
-  const comments = commentsArray.map((comment) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+  const required = (val) => val && val.length;
+  const maxLength = (len) => (val) => !val || val.length <= len;
+  const minLength = (len) => (val) => !val || val.length >= len;
+
+  const handleSubmit = (values) => {
+    console.log("Current State is: " + JSON.stringify(values));
+    alert("Current State is: " + JSON.stringify(values));
+    // event.preventDefault();
+  }
+
+  const commentForm = () => {
     return (
-      <div key={comment.id} className="col-12">
-        <h4>Comment</h4>
-        <ul className="list-unstyled">
-          <li>
-            <p>{comment.comment}</p>
-          </li>
-          <li></li>
-          <li>
-            <p>
-              --{comment.author} ,{" "}
-              {new Intl.DateTimeFormat("en-US", {
-                year: "numeric",
-                month: "short",
-                day: "2-digit",
-              }).format(new Date(Date.parse(comment.date)))}
-            </p>
-          </li>
-        </ul>
+      <div className="col-12 m-1">
+        <Button className="btn btn-light" onClick={toggleModal}>
+          <span className="fa fa-pencil fa-lg m-1"></span>Submit comment
+        </Button>
       </div>
     );
+  };
+  const comments = commentsArray.map((comment) => {
+    return (
+      <>
+        <div key={comment.id} className="col-12">
+          <h4>Comment</h4>
+          <ul className="list-unstyled">
+            <li>
+              <p>{comment.comment}</p>
+            </li>
+            <li></li>
+            <li>
+              <p>
+                --{comment.author} ,{" "}
+                {new Intl.DateTimeFormat("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "2-digit",
+                }).format(new Date(Date.parse(comment.date)))}
+              </p>
+            </li>
+          </ul>
+        </div>
+      </>
+    );
   });
-  return <div>{comments}</div>;
+  return (
+    <div>
+      <Modal isOpen={isModalOpen} toggle={toggleModal}>
+        <ModalHeader toggle={toggleModal}>Submit comment</ModalHeader>
+        <ModalBody>
+          <LocalForm onSubmit={(values) => handleSubmit(values)}>
+            <Row className="form-group">
+              <Col md={{ size: 12 }}>
+                <Label htmlFor="contactType">
+                  Rating
+                </Label>
+                <Control.select
+                  model=".contactType"
+                  name="contactType"
+                  className="form-control"
+                >
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </Control.select>
+              </Col>
+            </Row>
+            <Row className="form-group">
+              
+              <Col md={12}>
+              <Label htmlFor="name">
+                Your name
+              </Label>
+                <Control.text
+                  model=".name"
+                  id="name"
+                  name="name"
+                  placeholder="Your Name"
+                  className="form-control"
+                  validators={{
+                    required,
+                    minLength: minLength(3),
+                    maxLength: maxLength(15),
+                  }}
+                />
+                <Errors
+                  className="text-danger"
+                  model=".name"
+                  show="touched"
+                  messages={{
+                    required: "Required",
+                    minLength: "Must be greater than 2 characters",
+                    maxLength: "Must be 15 characters or less",
+                  }}
+                />
+              </Col>
+            </Row>
+            <Row className="form-group">
+              <Col md={12}>
+              <Label htmlFor="message">
+                Comment
+              </Label>
+                <Control.textarea
+                  model=".message"
+                  id="message"
+                  name="message"
+                  rows="6"
+                  className="form-control"
+                />
+              </Col>
+            </Row>
+            <Row className="form-group">
+              <Col md={{ size: 3}}>
+                <Button type="submit" color="primary">
+                  Submit
+                </Button>
+              </Col>
+            </Row>
+          </LocalForm>
+        </ModalBody>
+      </Modal>
+      <div>{comments}</div>
+      {commentForm()}
+    </div>
+  );
 };
+
 export const DishDetail = (props) => {
   if (props.dish != null) {
     return (
@@ -65,6 +179,7 @@ export const DishDetail = (props) => {
 
           <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
         </Breadcrumb>
+
         <div className="col-12">
           <h3>{props.dish.name}</h3>
           <hr />
--- 2-Front-End Web Development with React/React/confusion/src/components/MenuComponent.js
+++ 2-Front-End Web Development with React/React/confusion/src/components/MenuComponent.js
@@ -1,53 +1,55 @@
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    Card,
-    CardImg,
-    CardText,
-    CardBody,
-    CardTitle,
-    CardImgOverlay,
-  } from "reactstrap";
-import {Link} from 'react-router-dom';
+  Button,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardImgOverlay,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
-const RenderMenuItem = ({dish, onClick}) =>{
+const RenderMenuItem = ({ dish, onClick }) => {
+  return (
+    <>
+    <Card>
+      <Link to={`/menu/${dish.id}`}>
+        <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+        <CardImgOverlay>
+          <CardTitle heading>{dish.name}</CardTitle>
+        </CardImgOverlay>
+      </Link>
+    </Card>
+    </>
+  );
+};
+
+export const Menu = (props) => {
+  const menu = props.dishes.map((dish) => {
     return (
-          <Card>
-            <Link to = {`/menu/${dish.id}`}>
-            <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
-            <CardImgOverlay>
-              <CardTitle heading>{dish.name}</CardTitle>
-            </CardImgOverlay>
-            </Link>
-          </Card>
-      );
-}
+      <div key={dish.id} className="col-12 col-md-5 m-1">
+        <RenderMenuItem dish={dish} />
+      </div>
+    );
+  });
 
-export const Menu = (props) =>{
-    const menu = props.dishes.map((dish) => {
-        return(
-        <div key={dish.id} className="col-12 col-md-5 m-1">
-            <RenderMenuItem
-                dish={dish}
-            />
+  return (
+    <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/home">Home</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>Menu</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>Menu</h3>
+          <hr />
         </div>
-        );
-      });
-      
-      return (
-        <div className="container">
-          <div className="row">
-            <Breadcrumb>
-              <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-              <BreadcrumbItem active>Menu</BreadcrumbItem>
-            </Breadcrumb>
-            <div className="col-12">
-              <h3>Menu</h3>
-              <hr />
-            </div>
-          </div>
-          <div className="row">{menu}</div>
-        </div>
-      );
-
-}
+      </div>
+      <div className="row">{menu}</div>
+    </div>
+  );
+};
