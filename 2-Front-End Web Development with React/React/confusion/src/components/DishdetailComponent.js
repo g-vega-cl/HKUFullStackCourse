02b036f56d97ea8c545a76d2e5d295c29b4cff96
g-vega-cl <g.vega.cl@gmail.com>
2021-02-14 13:57:24 -0600

    Fetch Post Comment

--- 2-Front-End Web Development with React/React/confusion/src/components/DishdetailComponent.js
+++ 2-Front-End Web Development with React/React/confusion/src/components/DishdetailComponent.js
@@ -38,7 +38,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     alert("Current State is: " + JSON.stringify(values));
-    this.props.addComment(
+    this.props.postComment(
       this.props.dishId,
       values.rating,
       values.author,
@@ -174,7 +174,7 @@ function RenderDish({ dish, isLoading, errMess }) {
   }
 }
 
-function RenderComments({ comments, addComment, dishId }) {
+function RenderComments({ comments, postComment, dishId }) {
   if (comments != null) {
     return (
       <div className="col-12 col-md-5 m-1">
@@ -197,7 +197,7 @@ function RenderComments({ comments, addComment, dishId }) {
             );
           })}
         </ul>
-        <CommentForm dishId={dishId} addComment={addComment} />
+        <CommentForm dishId={dishId} postComment={postComment} />
       </div>
     );
   } else return <div></div>;
@@ -240,7 +240,7 @@ const DishDetail = (props) => {
           <RenderDish dish={props.dish} />
           <RenderComments
             comments={props.comments}
-            addComment={props.addComment}
+            postComment={props.postComment}
             dishId={props.dish.id}
           />
         </div>
