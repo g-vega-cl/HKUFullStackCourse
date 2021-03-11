52cf2696b3f34ec9d60e8931e514ceff51c5a706
g-vega-cl <g.vega.cl@gmail.com>
2021-03-11 16:28:07 -0600

    Added basic node server with http

--- 4-ServersideDevelopmentNodeExpress/NodeJS/node-examples/index.tsx
+++ 4-ServersideDevelopmentNodeExpress/NodeJS/node-examples/index.tsx
@@ -1,16 +1,23 @@
 var rect = require('./rectangle.tsx');
 
 function solveRect(l,b){
-    console.log("Solving for rectangle with l = " + l + "and b = " + b);
+    console.log("Solving for rectangle with l = " + l + " and b = " + b);
+    
+    rect(l,b, (err,rectangle) => {
+        if(err){
+            console.log("Error ", err.message)
+        } else{
+            console.log("The area of the rectangle of dimensions l = " + l + " and b = " + b + " is " + rectangle.area())
 
-    if( l <= 0 || b <= 0){
-        console.log("Rectangle dimensions should be greater than 0")
-    } else {
-        console.log("The area of the rectangle is " + rect.area(l,b))
-        console.log("The perimeter of the rectangle is " + rect.perimeter(l,b))
-    }
+            console.log("The perimeter of the rectangle of dimensions l = " + l + " and b = " + b + " is " + rectangle.perimeter())
+        }
+
+        console.log("We called rect already")
+        
+    })
+
 }
 
 solveRect(2,4)
-solveRect(4,1)
-solveRect(4,-1)
+// solveRect(4,1)
+// solveRect(4,-1)
