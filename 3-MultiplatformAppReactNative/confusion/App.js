6c5ce589253f52cc607964db7fedbf47fe70c2c5
g-vega-cl <g.vega.cl@gmail.com>
2021-03-11 13:43:29 -0600

    Added simple node example

--- 3-MultiplatformAppReactNative/confusion/App.js
+++ 3-MultiplatformAppReactNative/confusion/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import Main from "./components/MainComponent";
+import {  } from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
-export default function App() {
+const MenuNavigator = createStackNavigator({
+  Menu: {screen: Menu},
+  Dishdetail: {screen: Dishdetail}
+},{
+  initialRouteName: 'Menu',
+  navigationOptions: {
+    headerStyle:{
+      backgroundColor: '#512DA88'
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      color: '#fff'
+    }
+  }
+})
+
+
+function MyApp() {
   return (
     <Main/>
   );
 }
+const App = createAppContainer(MyApp);
+
+
+export default App
--- 3-MultiplatformAppReactNative/confusion/components/MenuComponent.js
+++ 3-MultiplatformAppReactNative/confusion/components/MenuComponent.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
 
-export default Menu = ({dishes}) =>{
-    
+export default Menu = ({dishes, navigation}) =>{
+    navigationOptions = {
+        title: 'Menu'
+    }
+
+    const {navigate} = navigation;
+
     const renderMenuItem = ({item,index, onPress}) => {
         return <ListItem
             key = {index}
             title = {item.name}
             subtitle = {item.description}
             hideChevron = {true}
-            onPress = {() => onPress(item.id)}
-            leftAvatar = {{source: require("./images/uthapizza.png")}}
+            onPress = {() => navigate('Dishdetail', {dishId: item.id})}
+            leftAvatar = {{source: require("./images/uthappizza.png")}}
         />
     }
+
+            {/* <Menu dishes={dishes} onPress={(dishId) => onDishSelect(dishId)} />
+        <DishDetail dish = {dishes.filter((dish) => dish.id === selectedDish)[0]}/> */}
 
     return(
         <FlatList
