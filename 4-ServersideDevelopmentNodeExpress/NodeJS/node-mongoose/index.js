f533f1d61cfb52db72857118164bce6da7567327
g-vega-cl <g.vega.cl@gmail.com>
2021-03-15 22:19:51 -0600

    Mongoose part 2

--- 4-ServersideDevelopmentNodeExpress/NodeJS/node-mongoose/index.js
+++ 4-ServersideDevelopmentNodeExpress/NodeJS/node-mongoose/index.js
@@ -1,32 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Dishes = require('./models/dishes');
+const Dishes = require("./models/dishes");
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = "mongodb://localhost:27017/conFusion";
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
-    console.log("Connected to server");
-    var newDish = Dishes({
-        name: 'Uthappizza',
-        description: 'test'
+  console.log("Connected to server");
+
+  Dishes.create({
+    name: 'Uthappizza',
+    description: 'test'
+})
+.then((dish) => {
+    console.log(dish);
+
+    return Dishes.findByIdAndUpdate(dish._id, {
+        $set: { description: 'Updated test'}
+    },{ 
+        new: true 
+    })
+    .exec();
+})
+.then((dish) => {
+    console.log(dish);
+
+    dish.comments.push({
+        rating: 5,
+        comment: 'I\'m getting a sinking feeling!',
+        author: 'Leonardo di Carpaccio'
     });
 
-    newDish.save()
-    .then((dish) => {
-        console.log(dish);
-        
-        return Dishes.find({}).exec();
-    })
-    .then((dishes) => {
-        console.log(dishes);
+    return dish.save();
+})
+.then((dish) => {
+    console.log(dish);
 
-        return Dishes.remove({});
-    })
-    .then(() =>{
-        return mongoose.connection.close();
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+    return Dishes.remove({});
+})
+.then(() => {
+    return mongoose.connection.close();
+})
+.catch((err) => {
+    console.log(err);
+});
 });
--- 4-ServersideDevelopmentNodeExpress/NodeJS/node-mongoose/models/dishes.js
+++ 4-ServersideDevelopmentNodeExpress/NodeJS/node-mongoose/models/dishes.js
@@ -1,6 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    rating:{
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true,
+    }, 
+    comment:{
+        type: String,
+        required: true
+    },
+    author:{
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+});
+
 const dishSchema = new Schema(
   {
     name: {
@@ -12,6 +31,7 @@ const dishSchema = new Schema(
       type: String,
       require: true,
     },
+    comments: [commentSchema],
   },
   {
     timestamps: true,
